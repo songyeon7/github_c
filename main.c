e5d6fb5eda39b5b045cbad3275d2bce2cf180c8b
songyeon7 <97648066+songyeon7@users.noreply.github.com>
2022-12-20 11:27:26 +0900

    inital commit

--- main.c
+++ main.c
@@ -4,6 +4,21 @@
 
 int main(){
     srand(time(NULL));
-    int question = 10;
+    
     int question_try = 1;
+    int num1, num2;
+
+    num1 = rand()%10;
+    num2 = rand()%10;
+
+    int calculate = rand()%4;
+    char sign;
+    switch (calculate)
+    {
+    case 1: sign = '+';break;
+    case 2: sign = '-';break;
+    case 3: sign = '*';break;
+    case 4: sign = '/';break;
+    }
+    
 }
